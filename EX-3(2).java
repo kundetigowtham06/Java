129ee6f0428a5fde075900af41bac2758fda470c
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-22 12:08:47 +0530

    Create EX-3(2).java

---
+++ EX-3(2).java
@@ -0,0 +1,16 @@
+import java.util.*;
+class D{
+	static void display(){
+	System.out.println("I am a CSE-D Student");
+	}
+}
+class C extends D{
+	static void display()
+	{
+	System.out.println("I am a CSE-C student");
+	}
+	public static void main(String args[]){
+	display();
+	}
+	
+}
