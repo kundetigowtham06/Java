903cedf45eed0510a00c6d0f7f1807673b87e2e3
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2024-09-26 08:46:31 +0530

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,43 @@
+
+abstract class Shape {
+    
+    abstract double Area();
+}
+
+class Circle extends Shape {
+    double r;
+
+  
+    Circle(double r) {
+        this.r = r;
+    }
+
+    
+    double Area() {
+        return 3.14 *r* r;
+    }
+}
+
+
+class Rectangle extends Shape {
+    int l, w;
+    Rectangle(int l,int w) {
+        this.l= l;
+        this.w= w;
+    }
+
+    double Area() {
+        return l*w;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        System.out.println("Area of Circle: " + circle.Area());
+        System.out.println("Area of Rectangle: " + rectangle.Area());
+    }
+}
