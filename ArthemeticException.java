2a22b93a051333c560c8e4d9ed7fd52fab27a41a
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2024-10-17 10:15:26 +0530

    Create ArthemeticException.java

---
+++ ArthemeticException.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class Hello{
+    public static void main(String args[]){
+        try{
+            int a=10/0;
+            System.out.println(a);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
