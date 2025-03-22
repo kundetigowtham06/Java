487b539b21ccb6bd53efc98fc896e7b8353feee3
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2025-03-22 16:22:33 +0530

    Update Knapsack.java

--- DataStructures/Dynamic Programming/Knapsack.java
+++ DataStructures/Dynamic Programming/Knapsack.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Knapsack2{
+public class Knapsack{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of Objects:");
