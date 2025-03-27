a4cfaedfba5c92166453f9f0726293068a2a7f7e
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2025-03-27 17:01:48 +0530

    Update QuickSort.java

--- DataStructures/Sort/QuickSort.java
+++ DataStructures/Sort/QuickSort.java
@@ -1,44 +1,55 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class QuickSort {
-    public static void swap(int a[],int i,int j){
-        int t=a[i];
-        a[i]=a[j];
-        a[j]=t;
+    public static void sort(int a[], int low, int high) {
+        if (low < high) {
+            int j = partition(a, low, high);
+            sort(a, low, j - 1);
+            sort(a, j + 1, high);
+        }
     }
-    public static int partion(int a[],int low,int high){
-        int p=a[low];
-        int i=low+1;
-        int j=high-1;
-        while(a[i]<=p && i<=j){
-            i=i+1;
+
+    public static int partition(int a[], int low, int high) {
+        int p = a[low];  
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= high && a[i] <= p) {
+                i++;
+            }
+            while (a[j] > p) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(a, i, j);
+            }
         }
-        while(a[j]>=p && i<=j){
-            j=j-1;
-        }
-        if(i<j){
-            swap(a, i, j);
-        }
-        swap(a,j,low);
+
+        swap(a, low, j); 
         return j;
     }
-    public static void sort(int a[],int low,int high){
-        if(low<high){
-            int j=partion(a, low, high);
-            sort(a,low,j-1);
-            sort(a,j+1,high);
+
+    public static void swap(int a[], int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        sort(a, 0, n - 1);
+
+        for (int i : a) {
+            System.out.print(i + " ");
         }
     }
-    public static void main(string args[]){
-        Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
-       int a[]=new int[n];
-       for(int i=0;i<n;i++){
-        a[i]=sc.nextInt();
-       } 
-       sort(a,0,n-1);
-       for(int i:a){
-        System.out.print(i+" ");
-       }
-    }
 }
+
