c2f385d4367484a2246724be3555aee98cf1d71a
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2025-04-14 11:36:07 +0530

    Update MinHeap.java

--- DataStructures/Heap/MinHeap.java
+++ DataStructures/Heap/MinHeap.java
@@ -1,84 +1,91 @@
-class MinHeap{
-    static int a[];
-    static int heapsize;
-   static int maxsize;
-    MinHeap(int ms){
-        maxsize=ms;
-        heapsize=0;
+class MinHeap {
+    int a[];
+    int heapsize;
+    int maxsize;
+
+    MinHeap(int ms) {
+        maxsize = ms;
+        heapsize = 0;
         a = new int[maxsize];
     }
-    public static int parent(int i){
-        return (i-1)/2;
+
+    int parent(int i) {
+        return (i - 1) / 2;
     }
-    public static int lchild(int i){
-        return 2*i+1;
+
+    int lchild(int i) {
+        return 2 * i + 1;
     }
-    public static int rchild(int i){
-        return 2*i+2;
+
+    int rchild(int i) {
+        return 2 * i + 2;
     }
-    public static void insert(int key){
-        if(heapsize==maxsize){
+
+    void insert(int key) {
+        if (heapsize == maxsize) {
             System.out.println("heap overflow");
             return;
         }
         int i = heapsize;
-        a[i]=key;
+        a[i] = key;
         heapsize++;
-        while(i!=0&&a[i]<a[parent(i)]){
+        while (i != 0 && a[i] < a[parent(i)]) {
             int temp = a[i];
             a[i] = a[parent(i)];
             a[parent(i)] = temp;
-           i = parent(i);
+            i = parent(i);
         }
     }
-    public static void heapify(int i){
+
+    void heapify(int i) {
         int min = i;
-       int l= lchild(min);
-       int r=  rchild(min);
-        if(l<heapsize&& a[l]<a[min])
-            min=l;
-        else if(r<heapsize&& a[r]<a[min])
-            min=r;
-        if(min!=i){
+        int l = lchild(min);
+        int r = rchild(min);
+        if (l < heapsize && a[l] < a[min])
+            min = l;
+        if (r < heapsize && a[r] < a[min])
+            min = r;
+        if (min != i) {
             int temp = a[min];
-            a[min]= a[i];
+            a[min] = a[i];
             a[i] = temp;
             heapify(min);
         }
     }
-    public static void remove(){
-        if(heapsize==0){
+
+    void remove() {
+        if (heapsize == 0) {
             System.out.println("underflow");
+            return;
         }
-        if(heapsize==1){
-          heapsize--;
-        }
-        else{
-            a[0] = a[heapsize-1];
+        if (heapsize == 1) {
+            heapsize--;
+        } else {
+            a[0] = a[heapsize - 1];
             heapsize--;
             heapify(0);
-
         }
-
     }
-    public static int getMax(){
+
+    int getMin() {
         return maxsize;
     }
-    public static int currmax(){
+
+    int currmin() {
         return heapsize;
     }
+
     public static void main(String[] args) {
         MinHeap m = new MinHeap(15);
-        int ele[] = {1,2,3,4,5};
-        for(int i:ele){
-            insert(i);
+        int ele[] = {1, 2, 3, 4, 5};
+        for (int i : ele) {
+            m.insert(i);
         }
-        remove();
-        for(int j =0;j<heapsize;j++){
-            System.out.println(a[j]);
+        m.remove();
+        for (int j = 0; j < m.heapsize; j++) {
+            System.out.println(m.a[j]);
         }
-        System.out.println("curr max :"+currmax());
-        System.out.println("maxsize :"+getMax());
+        System.out.println("curr min :" + m.currmin());
+        System.out.println("minsize :" + m.getMin());
     }
-
 }
